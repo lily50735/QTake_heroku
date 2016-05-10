1aac67fd011388e8443c81fb58adb677fa361229
Lily Yu <chy59@pitt.edu>
2016-05-10 18:40:42 -0400

    heroic

--- Gruntfile.js
+++ Gruntfile.js
@@ -317,7 +317,23 @@ module.exports = function (grunt) {
       return grunt.task.run(testTasks);
     }
   });
-
+    copy: {
+        dist: {
+            files: [{
+                expand: true,
+                dest: '<%= yeoman.dist %>',
+                cwd: 'heroku',
+                src: '*',
+                rename: function(dest, src) {
+                    var path = require('path');
+                    if (src === 'distpackage.json') {
+                        return path.join(dest, 'package.json');
+                    }
+                    return path.join(dest, src);
+                }
+            }]
+        }
+    }
   grunt.registerTask('build', [
     'clean:dist',
     'createDefaultTemplate',
